98b9bf38e67f5b363d0f620ec249e799ad72931c
Taegon <josemirdel.23@gmail.com>
2026-09-08 14:03:12 -0600

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'v1_cache_pwa';
+// Incluye solo las rutas que sí existen en tu GitHub
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
